aa7a76a84158981b7b86a6f35a8750f656dcefca
ssmt <ssmt@tut.by>
2021-08-21 19:41:49 +0300

    1st

---
+++ by/tut/ssmt/MyArrayList.java
@@ -0,0 +1,35 @@
+package by.tut.ssmt;
+
+
+public class MyArrayList {
+        private Object[] arr;
+        private int size;
+        private int defaultSize = 10;
+
+    public MyArrayList() {
+        arr = new Object[defaultSize];
+    }
+
+
+
+    public void trimToSize(){
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Object get(int index){
+    return arr[index];
+    }
+
+    public void add(Object ob){
+
+    }
+
+    public void remove (int index) {
+
+    }
+
+}
